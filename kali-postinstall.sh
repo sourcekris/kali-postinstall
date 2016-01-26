586122a3b88f1d4ccfbe24a1a19c42e5b119a93f
root <sewid666@gmail.com>
2016-01-25 22:00:26 -0800

    Added some environment checks, added some more gsettings to disable help shortcut

--- kali-postinstall.sh
+++ kali-postinstall.sh
@@ -1,49 +1,90 @@
 #!/bin/bash
-#
+#-Metadata-----------------------------------------------------------------
+# Filename: kali-postinstall.sh
+# Date: 2016-01-26
+#-Notes--------------------------------------------------------------------
 # These are the things I do after install Kali 2016.1 on a new VM/System. 
+#
+# Run this as root after an install of Kali 2016.
 # 
-# This is provided as-is and is not meant for others. However, you might find
-# some of this stuff useful.
+# This is provided as-is and is not meant for others. However, you might 
+# find some of this stuff useful. Got some of these ideas from g0tm1lk,
+# see his script at:
+#
+# https://github.com/g0tmi1k/os-scripts/blob/master/kali.sh
 #
 # Tweet @CTFKris for ideas to add to this.
 #
-# Updated: 26-Jan-16
-#
 
-echo "[*] Improving Kali 2016.1 ..."
-echo "[+] Setting local Aussie Kali mirror (mirror.aarnet.edu.au) ..."
-sed -i "s/http\.kali\.org/mirror\.aarnet\.edu\.au\/pub\/kali/" /etc/apt/sources.list
+# Path to download packages, XPI's etc to
+SCRIPTDLPATH="scriptdls/"
+
+# Kali mirror you prefer, Australians can use AARNet
+KALIMIRROR="mirror.aarnet.edu.au/pub/kali"
+
+# We do VM detection later, default case it false, set manually to true if the 
+# detection fails for you
+VM=false
+
+# Check we're root
+if [ $EUID -ne 0 ]
+then
+	echo "[-] This script must be run as root." 
+	exit 1
+fi
+
+echo "[*] Improving Kali 2016.1"
+
+if [ `dmidecode | grep -iq virtual` -gt 0 ]
+then
+	VM=true
+fi
+
+echo "[+] Setting preferred Kali mirror - $KALIMIRROR ..."
+sed -i s/http\.kali\.org/$KALIMIRROR/ /etc/apt/sources.list
 echo "[+] Updating repos from new mirror..."
 apt-get -qq update
-echo "[+] Installing openvm tools and mate desktop..."
-apt-get -y -qq install open-vm-tools-desktop fuse mate-core mate-desktop-environment-extra mate-desktop-environment-extras 
+
+if [ "$VM" == "true" ]
+then
+	echo "[+] Installing open-vm-tools..."
+	apt-get -y -qq install open-vm-tools-desktop fuse 
+else
+	echo "[*] Virtual machine NOT detected, skipping vmtools installation..."
+fi
+echo "[+] Installing mate desktop and setting it to default Xsession..."
+apt-get -y -qq install mate-core mate-desktop-environment-extra mate-desktop-environment-extras 
 echo mate-session > ~/.xsession
-echo "[+] Downloading themes and fonts..."
-mkdir themedls 2>/dev/null
-wget -q -P themedls/ http://ftp.iinet.net.au/pub/ubuntu/pool/main/u/ubuntu-themes/ubuntu-mono_14.04+15.10.20151001-0ubuntu1_all.deb
-wget -q -P themedls/ http://ftp.iinet.net.au/pub/ubuntu/pool/main/h/humanity-icon-theme/humanity-icon-theme_0.6.10_all.deb
-wget -q -P themedls/ http://ftp.iinet.net.au/pub/ubuntu/pool/main/u/ubuntu-font-family-sources/ttf-ubuntu-font-family_0.83-0ubuntu1_all.deb
-wget -q -P themedls/ https://launchpad.net/~ravefinity-project/+archive/ubuntu/ppa/+files/ambiance-colors_15.10.1~wily~NoobsLab.com_all.deb
+
+echo "[+] Downloading Ambiance themes and fonts..."
+mkdir scriptdls 2>/dev/null
+wget -q -P "$SCRIPTDLPATH" http://ftp.iinet.net.au/pub/ubuntu/pool/main/u/ubuntu-themes/ubuntu-mono_14.04+15.10.20151001-0ubuntu1_all.deb
+wget -q -P "$SCRIPTDLPATH" http://ftp.iinet.net.au/pub/ubuntu/pool/main/h/humanity-icon-theme/humanity-icon-theme_0.6.10_all.deb
+wget -q -P "$SCRIPTDLPATH" http://ftp.iinet.net.au/pub/ubuntu/pool/main/u/ubuntu-font-family-sources/ttf-ubuntu-font-family_0.83-0ubuntu1_all.deb
+wget -q -P "$SCRIPTDLPATH" https://launchpad.net/~ravefinity-project/+archive/ubuntu/ppa/+files/ambiance-colors_15.10.1~wily~NoobsLab.com_all.deb
+
 echo "[+] Installing themes and fonts..."
-cd themedls
+cd $SCRIPTDLPATH
 dpkg -i *.deb
-cd ..
-echo "[+] Cleaning up deb package dls..."
-rm -fr themedls
+cd $OLDPWD
+
 echo "[+] Downloading firefox extensions..."
-wget -q https://addons.mozilla.org/firefox/downloads/latest/310783/addon-310783-latest.xpi
-wget -q https://addons.mozilla.org/firefox/downloads/latest/3899/addon-3899-latest.xpi
-wget -q https://addons.mozilla.org/firefox/downloads/latest/92079/addon-92079-latest.xpi
-wget -q https://addons.mozilla.org/firefox/downloads/latest/472577/addon-472577-latest.xpi
-wget -q https://addons.mozilla.org/firefox/downloads/latest/51740/platform:5/addon-51740-latest.xpi
+wget -q -P "$SCRIPTDLPATH" https://addons.mozilla.org/firefox/downloads/latest/310783/addon-310783-latest.xpi
+wget -q -P "$SCRIPTDLPATH" https://addons.mozilla.org/firefox/downloads/latest/3899/addon-3899-latest.xpi
+wget -q -P "$SCRIPTDLPATH" https://addons.mozilla.org/firefox/downloads/latest/92079/addon-92079-latest.xpi
+wget -q -P "$SCRIPTDLPATH" https://addons.mozilla.org/firefox/downloads/latest/472577/addon-472577-latest.xpi
+wget -q -P "$SCRIPTDLPATH" https://addons.mozilla.org/firefox/downloads/latest/51740/platform:5/addon-51740-latest.xpi
+
 echo "[+] Downloading wallpaper to ~/Pictures/kalibg.png"
 wget -q -O ~/Pictures/kalibg.png http://img11.deviantart.net/1cda/i/2015/294/f/b/kali_2_0__not_official__wallpaper_by_xxdigipxx-d9dw004.png
 
-echo "[+] Installing more packages"
+echo "[+] Installing more packages..."
 apt-get -y -qq install gimp squashfs-tools pngcheck exiftool mongodb-clients 
-echo "[+] Installing pwntools"
+
+echo "[+] Installing pwntools..."
 pip install pwntools
-echo "[+] Installing PEDA"
+
+echo "[+] Installing PEDA..."
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 
@@ -58,12 +99,12 @@ wget -q -O ~/.msf5/plugins/pentest.rb https://raw.githubusercontent.com/darkoper
 echo "[+] Updating wpscan..."
 wpscan --update
 
-echo "[+] Upgrading packages..."
-apt-get -y -qq upgrade
-
-echo "[+] Updating mate settings"
-# Terminal and screensaver
+echo "[+] Updating mate settings..."
+# Terminal 
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ scrollback-unlimited true	# unlimited terminal scrollback
+gsettings set org.mate.terminal.keybindings help 'disabled' # hate hitting help accidently, noone cares
+
+# Disable screensavers!
 gsettings set org.mate.screensaver idle-activation-enabled false	# disable screensave
 gsettings set org.mate.power-manager sleep-display-ac 0				# disable screen sleeping when plugged in
 
@@ -84,7 +125,11 @@ gsettings set org.mate.Marco.general titlebar-font 'Ubuntu Medium 11'
 gsettings set org.mate.font-rendering antialiasing 'rgba'
 gsettings set org.mate.font-rendering hinting 'slight'
 
-echo "[+] Installing firefox extensions..."
+echo "[+] Upgrading packages..."
+APT_LISTCHANGES_FRONTEND=none apt-get -o Dpkg::Options::="--force-confnew" -y -qq upgrade
+
+echo "[+] Installing firefox extensions, go through the tabs and accept the installs..."
+cd "$SCRIPTDLPATH"
 firefox *.xpi
-rm -f *.xpi
+rm -fr "$SCRIPTDLPATH"
 echo "[*] You need to reboot for the vmtools to take effect."
